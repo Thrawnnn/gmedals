61a25884af70a7bf8a5c235a4656783d487722e4
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-07-12 22:28:05 -0500

    Add more default medals.

--- gMedals/lua/gMedals/sh_gmedals.lua
+++ gMedals/lua/gMedals/sh_gmedals.lua
@@ -6,11 +6,21 @@ gMedals.Config = gMedals.Config or {}
 RANK_OFFICER = 1
 
 gMedals.Config = {
-    [RANK_OFFICER] = {
-        name = "Officer",
+    [CADET_INSIGNIA] = {
+        name = "Cadet",
         desc = "This is a Default Medal!",
         material = "rank-1.png"
-    }
+    },
+    [SERGEANT_INSIGNIA] = {
+        name = "Sergeant",
+        desc = "This is a Default Medal!",
+        material = "rank-2.png"
+    },
+    [OFFICER_INSIGNIA] = {
+        name = "Officer",
+        desc = "This is a Default Medal!",s
+        material = "rank-3.png"
+    },
 }
 
 function p:GiveMedal(medalEnum)
