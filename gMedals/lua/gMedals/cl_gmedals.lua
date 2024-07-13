4c86dfd691efb8cb31c26a674b31114a133fbe61
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-07-12 22:20:47 -0500

    some shit!

--- gMedals/lua/gMedals/cl_gmedals.lua
+++ gMedals/lua/gMedals/cl_gmedals.lua
@@ -1,7 +1,7 @@
 -- HUD Shit probably
 
 
-local function DrawOverheadInfo(target, alpha)
+local function gMedal_Draw(target, alpha)
 	local pos = target:EyePos()
 
 	pos.z = pos.z + 5
@@ -10,7 +10,6 @@ local function DrawOverheadInfo(target, alpha)
 
 	local ang = (pos - lp:EyePos()):GetNormalized():Angle()
 
-	--
 	ang:RotateAroundAxis(ang:Up(), -106)
 	ang:RotateAroundAxis(ang:Forward(), 90)
 
@@ -20,38 +19,18 @@ local function DrawOverheadInfo(target, alpha)
 	pos = pos + ang:Forward() * (target.OffsetHorizontal3D2D or 8)
 
 	cam.Start3D2D(pos, ang, 0.1)
-	cam.IgnoreZ(true)
-        draw.DrawText("Test", "Arial", 3, 3, 0)
-	cam.IgnoreZ(false)
+		cam.IgnoreZ(true)
+        		draw.DrawText("Test", "Arial", 3, 3, 0)
+		cam.IgnoreZ(false)
 	cam.End3D2D()
 end
 
-hook.Add("PostDrawTranslucentRenderables", "GMedals_Draw", function(bDrawingDepth,bDrawingSkybox)
-	if bDrawingDepth or bDrawingSkybox then
+hook.Add("PostDrawTranslucentRenderables", "GMedals_Draw", function(bDP, bDS)
+	if bDP or bDS then
 		return
 	end
 
 	local lp = LocalPlayer()
 	local realTime = RealTime()
 	local frameTime = FrameTime()
-
-	for entTarg, shouldDraw in pairs(overheadEntCache) do
-		if IsValid(entTarg) then
-			local alpha =entTarg.overheadAlpha
-
-			if lastEnt != entTarg then
-				overheadEntCache[entTarg] = false
-			end
-
-			if alpha > 0 then
-				if not entTarg:GetNoDraw() then
-					if entTarg:IsPlayer() then
-						DrawOverheadInfo(entTarg, alpha)
-					end
-				end
-			end
-		else
-			overheadEntCache[entTarg] = nil
-		end
-	end
 end)
