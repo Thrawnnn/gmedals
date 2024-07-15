bf968a8c63e9599cbfc704b0321ba88e7a955472
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-07-14 23:41:05 -0500

    Delete lua directory

--- lua/nexus_library/functions/sh_functions.lua
+++
@@ -1,8 +0,0 @@
-local letters = "abcdefghijklmnopqrstuvwxyz"
-function Nexus:GenerateID(amount)
-    local str = ""
-    for i = 1, amount do
-        str = str..letters[math.random(1, #letters)]
-    end
-    return str
-end
